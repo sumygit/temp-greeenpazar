90f299b280a2b6e55d14d4d1c1119b80786d5538
AAA <sugithubx@gmail.com>
2026-05-26 08:19:50 +0300

    fix:1

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,22 +1,25 @@
-import Container from "../global/Container"
-import NavbarBasket from "./NavbarBasket"
-import NavbarLogo from "./NavbarLogo"
-import NavbarMenu from "./NavbarMenu"
-import NavbarSearch from "./NavbarSearch"
+import { Suspense } from "react";
+import Container from "../global/Container";
+import NavbarBasket from "./NavbarBasket";
+import NavbarLogo from "./NavbarLogo";
+import NavbarMenu from "./NavbarMenu";
+import NavbarSearch from "./NavbarSearch";
 
+function Navbar() {
+  return (
+    <header className="bg-brand-green">
+      <Container className="flex justify-between items-center gap-7">
+        <NavbarLogo />
+        <Suspense>
+          <NavbarSearch />
+        </Suspense>
 
-  function Navbar() {
-	return (
-		<header className="bg-brand-green">
-			<Container className="flex justify-between items-center gap-7">
-				<NavbarLogo/>
-				<NavbarSearch />
-				<div className="flex justify-between gap-3 items-center">
-					<NavbarBasket href="/"/>
-					<NavbarMenu/>
-				</div>
-			</Container>
-		</header>
-	)
+        <div className="flex justify-between gap-3 items-center">
+          <NavbarBasket href="/" />
+          <NavbarMenu />
+        </div>
+      </Container>
+    </header>
+  );
 }
-export default Navbar
+export default Navbar;
